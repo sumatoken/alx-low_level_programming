b485a5f7c76055572d22b11ef643a6f858d77a25
Mohammed Bermime <bermime1@gmail.com>
2023-03-16 16:06:42 -0700

    Introduction to C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,9 @@
  */
 int main(void)
 {
-        write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-        return (1);
+	write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar,
+		2015-10-19\n", 59);
+
+	return (1);
 }
 
