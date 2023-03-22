087fdc9a96c6e8472734c484021867b88cc29652
Mohammed Bermime <bermime1@gmail.com>
2023-03-22 15:11:01 -0700

    C debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,19 +1,11 @@
 #ifndef main_h
 #define main_h
 
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int r);
-int print_last_digit(int r);
-void jack_bauer(void);
-void times_table(void);
-int add(int a, int b);
-void print_to_98(int n);
-void print_times_table(int n);
-void putformat(int);
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int, int, int);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
 #endif
