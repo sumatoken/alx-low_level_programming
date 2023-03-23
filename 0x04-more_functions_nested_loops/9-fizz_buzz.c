da00de173193023ccebe6e5d37e72c1484954bdc
Mohammed Bermime <bermime1@gmail.com>
2023-03-23 11:09:15 -0700

    FizzBuzz in C

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,12 +11,12 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
-			printf("%s", "Fizz ");
+		if (i % 15 == 0)
+			printf("%s", "FizzBuzz ");
 		else if (i % 5 == 0)
 			printf("%s", "Buzz ");
-		else if (i % 15 == 0)
-			printf("%s", "FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("%s", "Fizz ");
 		else
 			printf("%d\t", i);
 	}
