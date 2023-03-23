58172b339b1a8c4d011d1c2ee7db5ac5e44de70f
Mohammed Bermime <bermime1@gmail.com>
2023-03-23 04:26:21 -0700

    C debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,13 +14,13 @@ void print_remaining_days(int month, int day, int year)
 {
 	if (year % 4 == 0)
 	{
-	printf("Day of the year leap: %d\n", day);
-	printf("Remaining days: %d\n", 366 - day);
+		printf("Day of the year leap: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
-	printf("Day of the year: %d\n", day);
-	printf("Remaining days: %d\n", 365 - day);
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 365 - day);
 	}
 }
 
