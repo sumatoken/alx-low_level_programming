cacc09ae8e1873089b7984c10d6b0d62843cc52b
Mohammed Bermime <bermime1@gmail.com>
2023-03-23 10:38:16 -0700

    More into C nested loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - a function that draws a straight line in the terminal.
+ * followed by a new line.
+ * @n: An input integer
+ * Return: Always 0
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (n == 0)
+			break;
+		_putchar("_");
+	}
+	_putchar('\n');
+}
