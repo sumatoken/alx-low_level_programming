7c7ecf4060c31769d3922ed5b6c28552cd79cd8f
Mohammed Bermime <bermime1@gmail.com>
2023-03-23 10:31:17 -0700

    More into C nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,16 +5,18 @@
    * followed by a new line.
    * Return: Always 0
  */
-
 void more_numbers(void)
 {
-	int i, j;
+	int i, j = 0;
 
-	for (i = 0; i <= 9; i++)
+	for (; j < 10; j++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (i = 0; i <= 14; i++)
 		{
-			_putchar(j);
+			if (i > 9)
+				_putchar(i / 10 + '0');
+
+			_putchar(i % 10 + '0');
 		}
 		_putchar('\n');
 	}
